0dda24344116ae1b1e8026e25a5c36bb12058233
Liviu Panainte <ubick@users.noreply.github.com>
2015-11-21 12:40:02 +0000

    Update apache2 cookbook to 3.1.0

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license           "Apache 2.0"
 description       "enable driving cookbooks that are not normally config driven to be so"
 version           "1.5.0"
 
-depends "apache2", "~> 1.8"
+depends "apache2", "~> 3.1.0"
 depends 'iptables-ng', '~> 2.2.0'
 depends "mysql", "~> 6.1"
 depends 'nginx', '< 2.4.4'
